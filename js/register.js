e12369821ce3b297d644d6a59aed3dc62a2e7572
RobyLedda94 <robyledda01@yahoo.it>
2026-02-05 16:07:48 +0100

    [FIX] nome file

---
+++ js/register.js
@@ -0,0 +1,9 @@
+
+// Recupero gli elementi dal dom
+let register_btn = document.getElementById("register_btn");
+
+// Aggiungo l'evento di click al bottone di registrazione
+register_btn.addEventListener('click', function(){
+    console.log("Cliccato il bottone di registrazione");
+});
+
